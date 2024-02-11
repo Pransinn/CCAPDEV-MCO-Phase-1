e7b07cf52ecfe49223499dee331dc27c4d894b91
luis miguel <157296084+weeg16@users.noreply.github.com>
2024-02-11 23:55:53 +0800

    Update sampledata.js

--- sampledata.js
+++ sampledata.js
@@ -1,7 +1,86 @@
 const sampleData = [
+    // February 1st
     {
-        date: "2024-02-12",
-        reservations: ["08:00 AM", "09:00 AM", "11:00 AM"]
-    }
-    // Add more reservation data as needed...
-];
+      lab: "Lab A",
+      date: "2024-02-01",
+      reservations: ["8:00 AM", "9:00 AM", "10:00 AM"]
+    },
+    {
+      lab: "Lab B",
+      date: "2024-02-01",
+      reservations: ["9:00 AM", "10:00 AM", "11:00 AM"]
+    },
+    {
+      lab: "Lab C",
+      date: "2024-02-01",
+      reservations: ["10:00 AM", "11:00 AM", "12:00 PM"]
+    },
+  
+    // February 2nd
+    {
+      lab: "Lab A",
+      date: "2024-02-02",
+      reservations: ["8:00 AM", "9:00 AM", "10:00 AM"]
+    },
+    {
+      lab: "Lab B",
+      date: "2024-02-02",
+      reservations: ["9:00 AM", "10:00 AM", "11:00 AM"]
+    },
+    {
+      lab: "Lab C",
+      date: "2024-02-02",
+      reservations: ["10:00 AM", "11:00 AM", "12:00 PM"]
+    },
+  
+    // February 3rd
+    {
+      lab: "Lab A",
+      date: "2024-02-03",
+      reservations: ["8:00 AM", "9:00 AM", "10:00 AM"]
+    },
+    {
+      lab: "Lab B",
+      date: "2024-02-03",
+      reservations: ["9:00 AM", "10:00 AM", "11:00 AM"]
+    },
+    {
+      lab: "Lab C",
+      date: "2024-02-03",
+      reservations: ["10:00 AM", "11:00 AM", "12:00 PM"]
+    },
+  
+    // February 4th
+    {
+      lab: "Lab A",
+      date: "2024-02-04",
+      reservations: ["8:00 AM", "9:00 AM", "10:00 AM"]
+    },
+    {
+      lab: "Lab B",
+      date: "2024-02-04",
+      reservations: ["9:00 AM", "10:00 AM", "11:00 AM"]
+    },
+    {
+      lab: "Lab C",
+      date: "2024-02-04",
+      reservations: ["10:00 AM", "11:00 AM", "12:00 PM"]
+    },
+  
+    // February 5th
+    {
+      lab: "Lab A",
+      date: "2024-02-05",
+      reservations: ["8:00 AM", "9:00 AM", "10:00 AM"]
+    },
+    {
+      lab: "Lab B",
+      date: "2024-02-05",
+      reservations: ["9:00 AM", "10:00 AM", "11:00 AM"]
+    },
+    {
+      lab: "Lab C",
+      date: "2024-02-05",
+      reservations: ["10:00 AM", "11:00 AM", "12:00 PM"]
+    },
+  ];
